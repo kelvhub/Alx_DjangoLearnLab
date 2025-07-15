dd71162ad276a4a0f3dae98b722b45777d66799b
noelkel <noelkelechivincent@gmail.com>
2025-07-15 14:07:57 +0100

    html

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import login_view, logout_view, register_view, home_view  # only import views that exist
-
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', home_view, name='home'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', register_view, name='register'),
+
+
 ]
