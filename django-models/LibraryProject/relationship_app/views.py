a29ad3ea59e9e0b03f82bdda1b27fdc286f0f52a
noelkel <noelkelechivincent@gmail.com>
2025-07-15 14:03:47 +0100

    html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
