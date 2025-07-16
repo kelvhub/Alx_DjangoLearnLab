afb96fe35fe8c47fe0df1ba196f07c897f9f079d
noelkel <noelkelechivincent@gmail.com>
2025-07-16 18:46:01 +0100

    last_one

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponse
+from .models import Book
+from .forms import BookForm
 
 
-def home_view(request):    return render(request, 'relationship_app/home.html')
+def home_view(request):
+    return render(request, 'relationship_app/home.html')
+
 
 def role_check(role):
     def check(user):
@@ -20,16 +24,22 @@ def member_view(request):
 @login_required
 @user_passes_test(role_check('Admin'))
 def admin_view(request):
-    return render(request, 'relationship_app/admin_dashboard.html')
+    if hasattr(request.user, 'profile') and request.user.profile.role == 'admin':
+        return render(request, 'relationship_app/admin_dashboard.html')
+    return HttpResponse("Unauthorized", status=401)
 
 @login_required
 @user_passes_test(role_check('Librarian'))
 def librarian_view(request):
-    return render(request, 'relationship_app/librarian_dashboard.html') 
+    if hasattr(request.user, 'profile') and request.user.profile.role == 'librarian':
+        return render(request, 'relationship_app/librarian_dashboard.html')
+    return HttpResponse("Unauthorized", status=401)
 
 @login_required
 @user_passes_test(role_check('Member'))
 def member_view(request):
-    return render(request, 'relationship_app/member_dashboard.html')    
+    if hasattr(request.user, 'profile') and request.user.profile.role == 'member':
+        return render(request, 'relationship_app/member_dashboard.html')
+    return HttpResponse("Unauthorized", status=401)
 
 
