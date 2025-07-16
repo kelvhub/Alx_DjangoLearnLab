be0e4f211af15496b9ebcd06131fb8ee9a311fbc
noelkel <noelkelechivincent@gmail.com>
2025-07-16 18:54:53 +0100

    task

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,11 +3,7 @@ from .views import admin_view, librarian_view, member_view, home_view
 from .import views
 
 urlpatterns = [
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
-    path('', home_view, name='home'),  # Default view can be set to admin_view or any other view
     path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:pk>/edit/', views.change_book, name='edit_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
 ]
