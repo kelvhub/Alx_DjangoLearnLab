6e055cf15886fd2c9c36acf3c4ed6a3b37e2c6ab
noelkel <noelkelechivincent@gmail.com>
2025-07-30 10:48:20 +0100

    authenric

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,13 +1,14 @@
-# api/urls.py
-
 from django.urls import include, path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-    path('', include(router.urls)),  
+    path('', include(router.urls())),
+    path('auth/token/', obtain_auth_token, name='api_token_auth'),  # Token authentication endpoint
 ]
