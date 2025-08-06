2868637d238f926cf92bed2efaa14aa2cd4d014f
noelkel <noelkelechivincent@gmail.com>
2025-08-06 23:29:11 +0100

    appii

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 
